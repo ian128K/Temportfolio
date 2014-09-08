26e3b95a253cbc2aa2ee37c41ab3fbe47aa6c7b6
Ian Adams <ian@128K.co>
2014-09-08 00:11:58 -0700

    Finalised design
    * Removed LESS, since I’m editing the CSS directly.
    * Added Skrollr for scrolling effects.
    * Wrote ‘about’ bio.

--- js/freelancer.js
+++ js/freelancer.js
@@ -4,23 +4,44 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+$(document).ready(function(){
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+	// Initialise Skrollr
+	if ($(window).width() > 767) {
+		skrollr.init({
+			render: function(data) {
+				//Debugging - Log the current scroll position.
+				//console.log(data.curTop);
+			}
+		});
+	};
+	$(window).on('resize', function () {
+		if ($(window).width() <= 767) {
+			skrollr.init().destroy(); // skrollr.init() returns the singleton created above
+		} else {
+			skrollr.init({
+				render: function(data) {
+					//Debugging - Log the current scroll position.
+					//console.log(data.curTop);
+				}
+			});
+		}
+	});
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$('.page-scroll a').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+	// Highlight the top nav as scrolling occurs
+	$('body').scrollspy({
+		target: '.navbar-fixed-top'
+	});
+
 });
